4bd9cbb10336ec87a2b381416af0b791c677a6c8
mukaitakuto <ssssideline@gmail.com>
2020-10-19 19:16:02 +0900

    課題修正

--- app/src/main/java/jp/techacademy/takuto/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/takuto/kotlinlog/Human.kt
@@ -3,8 +3,12 @@ package jp.techacademy.takuto.kotlinlog
 import android.util.Log
 
 open class Human: Animal ,Thinkable{
+    var hobby: String
     // 引数付きコンストラクタ
-    constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
+    constructor(name: String, age: Int, hobby: String):super(name, age){
+        this.name = name
+        this.age = age
+        this.hobby = hobby
     }
 
 
